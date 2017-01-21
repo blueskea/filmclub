a316615b4df8528c41403c2c023987d501f5e0a6
Violet <Violet@DESKTOP-VIDVGE7>
2017-01-21 09:47:05 +0800

    init

---
+++ src/test/java/cn/edu/hfnu/controller/UserController.java
@@ -0,0 +1,18 @@
+package cn.edu.hfnu.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller  
+@RequestMapping("/user")  
+public class UserController {  
+
+	
+    @RequestMapping("/showUser")  
+    public String toIndex(HttpServletRequest request,Model model){  
+    	return "user";
+    }
+}
